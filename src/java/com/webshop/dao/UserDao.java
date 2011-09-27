7b41d7819223933d4ad46e0ca7d85eb1a1d04aff
Robin Lindahl <robin.lindahl@live.se>
2011-09-27 11:18:13 +0200

    Uppdaterad UserDAO
    Signed-off-by: Robin Lindahl <robin.lindahl@live.se>

--- src/java/com/webshop/dao/UserDao.java
+++ src/java/com/webshop/dao/UserDao.java
@@ -9,5 +9,26 @@ package com.webshop.dao;
  * @author Petter
  */
 public class UserDao {
+    public static boolean login(String eMail, String password){
+        
+        return false;
+    }
     
+    public static void removeUser(String eMail){
+        
+    }
+    
+    public static void setRole(String eMail){
+        //TODO: Add role enum
+    }
+    
+    public static User getUser(String eMail){
+        //TODO: Add User stuff here
+    }
+    
+    public static bool addUser(User user){
+        //TODO:Adduserandcheck
+        //Gick det bra- ja/nej
+        return false;
+    }
 }
